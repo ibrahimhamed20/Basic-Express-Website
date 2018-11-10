a6f6f9eda031104e2dfc655317b3f4aebe1a65f4
Ibrahim Hamed <ibrahimhamed2016@hotmail.com>
2018-11-10 21:07:16 +0200

    Basic Express Website

---
+++ routes/contact.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.post('/send', function(req, res, next) {
+    res.send('Email Sent!')
+});
+
+module.exports = router;
